4a6662ad4752ad86394a98a12b245c17fcbc0669
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-14 12:43:41 -0300

    .

---
+++ main.js
@@ -0,0 +1,11 @@
+function laranja() {
+    document.getElementById("mudarcor").src="img/laranja.jpg";
+}
+
+function rosa() {
+    document.getElementById("mudarcor").src="img/rosa.jpg";
+}
+
+function roxo() {
+    document.getElementById("mudarcor").src="img/vinho.jpg";
+}
